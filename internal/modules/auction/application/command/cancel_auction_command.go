23ca0e9b82e9d5b19f0956a674e581d9b5593767
Cristiano Pacheco <chris.spb25@gmail.com>
2026-01-06 18:51:47 -0300

    feat: implement CancelAuctionCommand for handling auction cancellation with error handling and unit of work management

---
+++ internal/modules/auction/application/command/cancel_auction_command.go
@@ -0,0 +1,83 @@
+package command
+
+import (
+	"context"
+	"time"
+
+	"github.com/cristiano-pacheco/go-online-auction/internal/modules/auction/ports"
+	"github.com/cristiano-pacheco/go-online-auction/pkg/logger"
+)
+
+type CancelAuctionCommandInput struct {
+	AuctionID uint64
+}
+
+type CancelAuctionCommandOutput struct {
+	ID        uint64
+	ListingID uint64
+	State     string
+	StartTime time.Time
+	EndTime   time.Time
+	UpdatedAt time.Time
+}
+
+type CancelAuctionCommand struct {
+	uowFactory ports.AuctionUnitOfWorkFactory
+	logger     logger.Logger
+}
+
+func NewCancelAuctionCommand(
+	uowFactory ports.AuctionUnitOfWorkFactory,
+	logger logger.Logger,
+) *CancelAuctionCommand {
+	return &CancelAuctionCommand{
+		uowFactory: uowFactory,
+		logger:     logger,
+	}
+}
+
+func (c *CancelAuctionCommand) Execute(
+	ctx context.Context,
+	input CancelAuctionCommandInput,
+) (CancelAuctionCommandOutput, error) {
+	uow, err := c.uowFactory.Begin(ctx)
+	if err != nil {
+		c.logger.Error().Err(err).Msg("failed to begin unit of work")
+		return CancelAuctionCommandOutput{}, err
+	}
+	defer func() { _ = uow.Rollback(ctx) }()
+
+	auction, err := uow.AuctionRepository().FindByIDForUpdate(ctx, input.AuctionID)
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("auction_id", input.AuctionID).Msg("failed to find auction for update")
+		return CancelAuctionCommandOutput{}, err
+	}
+
+	err = auction.Cancel()
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("auction_id", input.AuctionID).Msg("failed to cancel auction")
+		return CancelAuctionCommandOutput{}, err
+	}
+
+	err = uow.AuctionRepository().Update(ctx, auction)
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("auction_id", input.AuctionID).Msg("failed to update auction")
+		return CancelAuctionCommandOutput{}, err
+	}
+
+	err = uow.Complete(ctx)
+	if err != nil {
+		c.logger.Error().Err(err).Uint64("auction_id", input.AuctionID).Msg("failed to complete unit of work")
+		return CancelAuctionCommandOutput{}, err
+	}
+
+	state := auction.State()
+	return CancelAuctionCommandOutput{
+		ID:        auction.ID(),
+		ListingID: auction.ListingID(),
+		State:     state.String(),
+		StartTime: auction.StartTime(),
+		EndTime:   auction.EndTime(),
+		UpdatedAt: auction.UpdatedAt(),
+	}, nil
+}
