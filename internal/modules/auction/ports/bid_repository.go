a5796ee071667a482cab908d8254ef10665eb7ef
Cristiano Pacheco <chris.spb25@gmail.com>
2026-01-07 12:02:46 -0300

    feat: update Create method in BidRepository to return persisted BidModel and error

--- internal/modules/auction/ports/bid_repository.go
+++ internal/modules/auction/ports/bid_repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type BidRepository interface {
-	Create(ctx context.Context, bid model.BidModel) error
+	Create(ctx context.Context, bid model.BidModel) (model.BidModel, error)
 	FindByID(ctx context.Context, id uint64) (model.BidModel, error)
 	FindByAuctionID(ctx context.Context, auctionID uint64) ([]model.BidModel, error)
 	FindTopBidsByAuctionID(ctx context.Context, auctionID uint64, limit int) ([]model.BidModel, error)
